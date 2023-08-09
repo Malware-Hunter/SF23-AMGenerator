6d35c5a0e64e127f77e6c172c5324e60a21a6376
K D <diego.unipampa@gmail.com>
2023-08-09 10:48:27 -0300

    updates

--- run_demo_app.sh
+++ run_demo_app.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-python3 amgenerator.py --download -npd 2 -azk [AZ_KEY] --extraction --label -vtk [VT_KEY] --reanalyze-time 1 --file input/sha256_05.txt
+pip install -r requirements.txt
+
+python3 amgenerator.py --download -npd 2 -azk fa08a4ad8d8c9d3c56236d27bd9b99bb83c66c3fd65642d496ea2cbd13d4e8a4 --extraction --label -vtk d211226fd8cd68e10170dbc053a5cf6ca73d73ba51587eca4908c47046a57f18 --reanalyze-time 1 --file input/sha256_05.txt
 
