32fb3a0404bb991fe9293d632341d4e277b9b1e0
K D <diego.unipampa@gmail.com>
2023-08-09 10:52:30 -0300

    updates

--- run_demo_app.sh
+++ run_demo_app.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+echo "==========================================================="
+echo -n "Installing Python requirements ... "
+
 pip install -r requirements.txt
 
+echo "done."
+echo "==========================================================="
+
+echo ""
+
+echo "==========================================================="
+echo "Running amgenerator.py ... "
+echo ""
+
 python3 amgenerator.py --download -npd 2 -azk fa08a4ad8d8c9d3c56236d27bd9b99bb83c66c3fd65642d496ea2cbd13d4e8a4 --extraction --label -vtk d211226fd8cd68e10170dbc053a5cf6ca73d73ba51587eca4908c47046a57f18 --reanalyze-time 1 --file input/sha256_05.txt
+
+echo ""
+echo "done."
+echo "==========================================================="
 
